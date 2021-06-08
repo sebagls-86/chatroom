1f9a4845b72214ca945bc1dbeb8da0e42fd82ecf
Seba <sebastianglopezsosa@gmail.com>
2021-06-08 12:58:03 -0300

    prueba fallida mongoose

---
+++ chatModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const chatsSchema = new mongoose.Schema({
+
+    name: {type:String, required: true},
+    message:{type:String, required: true},
+    
+})
+
+const Chats = mongoose.model('chats', chatsSchema)
+
+module.exports = Chats
---
+++ server.js
@@ -0,0 +1,47 @@
+const chatController = require('./chatController')
+
+const {
+    Socket
+} = require("dgram")
+let express = require("express")
+const {
+    mongoose
+} = require("mongoose")
+let app = express()
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+const cors = require('cors')
+
+require('dotenv').config()
+require('./database')
+app.use(cors())
+
+app.use(express.static(__dirname))
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+var messages = [
+{name: "John", message: "Hello"}
+
+
+]
+
+const router = express.Router()
+
+router.route('/messages')
+.post(chatController.loadChats)
+.get(chatController.listChats)
+
+module.exports=router
+
+
+io.on('connection', (socket) => {
+
+    console.log('user connected')
+})
+
+let server = http.listen(3010, () => {
+    console.log("Server listening on port ", server.address().port)
+})
